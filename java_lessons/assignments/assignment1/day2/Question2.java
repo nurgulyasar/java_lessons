082ec50f56cde402463aef9631f663e9faa0fb15
nurgulyasar <90514275+nurgulyasar@users.noreply.github.com>
2021-09-15 21:07:22 +0300

    Add files via upload

---
+++ java_lessons/assignments/assignment1/day2/Question2.java
@@ -0,0 +1,10 @@
+package java_lessons.assignments.day2;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Turing" + "," + " Alan");
+	}
+
+}
---
+++ java_lessons/assignments/assignment1/day2/Question4.java
@@ -0,0 +1,10 @@
+package java_lessons.assignments.day2;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		System.out.println("Q");
+		
+	}
+
+}
