c515f94b6badaab8ac4576f11e3a7a2463d9d94d
nurgulyasar <90514275+nurgulyasar@users.noreply.github.com>
2021-09-15 21:12:15 +0300

    Add files via upload

--- java_lessons/assignments/assignment1/day2/Question7.java
+++ java_lessons/assignments/assignment1/day2/Question7.java
@@ -8,6 +8,7 @@ public class Question7 {
 			System.out.println("/__\\ /__\\");
 			System.out.println("");
 			System.out.println( "I am from \"Germany\".");
+			System.out.println();
 			System.out.println("How is your day going on,\'good\' or \'bad\'?");
 }
 	
