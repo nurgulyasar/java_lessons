f961f4d3e218a2d3c63d89bed470830cca8b5040
nurgulyasar <90514275+nurgulyasar@users.noreply.github.com>
2021-09-28 22:18:48 +0300

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,19 @@
+package day2_printing_comments;
+
+public class assignment {
+
+	int gallon;
+	
+	double liter;
+	
+	gallon=3.79*liter;
+	
+	
+	System.out.println (gallon+"gallon is "+liter+" liter");
+	
+	
+	
+	
+	
+	
+}
