1932913927281d774ba37fb1ef254ee8dc96e1f9
nurgulyasar <90514275+nurgulyasar@users.noreply.github.com>
2021-10-07 16:45:04 +0300

    Update question3_SwapNewValues.java

--- assignment_ArithmeticOperators/question3_SwapNewValues.java
+++ assignment_ArithmeticOperators/question3_SwapNewValues.java
@@ -4,22 +4,14 @@ public class question3_SwapNewValues {
 
 	public static void main(String[] args) {
 		
-		int Num1, Num2, swapNum1, swapNum2;
+		int first=5;
+		int second=10;
+		first=first+second;
+		second=first-second;
+		first=first-second;
 		
-		   Num1 = 10;
-		   Num2 = 20;
-		   
-		   System.out.println("n1="+ Num1);
-		   System.out.println("n2="+ Num2);
-		   
-		   System.out.println(" ");
-		   System.out.println("------");
-		   System.out.println(" ");
-		   
-		   swapNum1=Num2;
-		   System.out.println("n1=" + swapNum1);
-		   swapNum2=Num1;
-		   System.out.println("n2=" + swapNum2);
+		System.out.println("first: "+first);
+		System.out.println("second: "+second);
 		  
 
 	}
